36fd66ccdef049d2ed366bce075b72b90f422e93
Klhang <chrisjunhang@gmail.com>
2020-08-09 00:50:26 -0700

    test koa-router

--- test_koa.js
+++ test_koa.js
@@ -1,17 +1,34 @@
 const Koa = require('koa');
-const Router = require('koa-routere');
+const Router = require('koa-router');
 const bodyParser = require('koa-body');
 
 const app = new Koa();
+const router = new Router();
+const users = [];
 
 app.use(bodyParser({
     jsonLimit: 200
 }));
 
-app.use((ctx, next) => {
-    const { body } = ctx.request;
-    console.log(body);
-    ctx.body = 'got it';
-});
+router
+    .get('router name 1', '/user', async(ctx, next) => {
+        console.log(ctx.body)
+        ctx.body = { data: { users } };
+    })
+    .post('/user', async(ctx, next) => {
+        const { user } = ctx.request.body;
+        users.push(user);
+        ctx.body = { data: {users} };
+    });
+
+router
+    .get('/user/:id', async(ctx, next) => {
+        const name = ctx.params.name;
+        console.log(ctx.params);
+        const user = users.find(u => u.name === name);
+        ctx.body = { data : { user } };
+    });
+
+app.use(router.routes());
 
 app.listen('8080');
